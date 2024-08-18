448d5d84ddcd938935c419e25154670929f38d4f
Cesar <88-8@libe.com.mx>
2024-08-18 11:43:42 -0600

    Se agregan los elementos

--- back/build.gradle.kts
+++ back/build.gradle.kts
@@ -46,14 +46,13 @@ dependencies {
     implementation("org.springdoc:springdoc-openapi-ui:1.6.14")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
-    implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.mariadb.jdbc:mariadb-java-client:3.1.0") // Asegúrate de que la versión sea la más reciente.
     kapt("org.mapstruct:mapstruct-processor:$mapStructVersion")
     runtimeOnly("com.h2database:h2")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.security:spring-security-test")
+
     
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
