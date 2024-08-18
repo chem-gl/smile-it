92fb808e13df967c8ce8b71a63ec5394c04baec6
Cesar <88-8@libe.com.mx>
2024-08-18 10:57:54 -0600

    Se agrega los elementos del back

---
+++ back/src/main/kotlin/chemgl/smileit/SmileitApplication.kt
@@ -0,0 +1,11 @@
+package chemgl.smileit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SmileitApplication
+
+fun main(args: Array<String>) {
+	runApplication<SmileitApplication>(*args)
+}
---
+++ back/src/test/kotlin/chemgl/smileit/SmileitApplicationTests.kt
@@ -0,0 +1,13 @@
+package chemgl.smileit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SmileitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
